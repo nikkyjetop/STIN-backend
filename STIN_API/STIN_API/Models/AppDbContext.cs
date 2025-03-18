617868e06214d71e9c50125d63df3d3b371c7184
Dominika Strečanská <dominikastrecanska@gmail.com>
2025-03-18 14:58:23 +0100

    initial database connection

---
+++ STIN_API/STIN_API/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace STIN_API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ STIN_API/STIN_API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace STIN_API.Models
+{
+    public class User
+    {
+        public Int32 id { get; set; }
+        public string user_name { get; set; }
+    }
+}
